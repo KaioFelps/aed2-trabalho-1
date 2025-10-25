1a452741601726ad58f56648a1a58ed53c683cc6
Kaio Felps <kaiofelipe2005@gmail.com>
2025-10-24 23:41:55 -0300

    change(File): make id and path getters results constant

--- include/file.hpp
+++ include/file.hpp
@@ -14,8 +14,8 @@ private:
 public:
   File(std::string id, std::string path);
 
-  std::string &get_id() const noexcept;
-  std::string &get_path() const noexcept;
+  const std::string &get_id() const noexcept;
+  const std::string &get_path() const noexcept;
 
   void set_path(std::string path) noexcept;
 
