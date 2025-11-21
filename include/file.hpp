ae5e26f5473ba8a64d458761d93267bd1702af9d
Kaio Felps <kaiofelipe2005@gmail.com>
2025-11-21 19:50:00 -0300

    feat(File): implements `<` operator and add a comparator class to allow files to be stored in sets

--- include/file.hpp
+++ include/file.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include <string>
 
 namespace core
@@ -16,6 +17,13 @@ public:
   const std::string &get_path() const noexcept;
 
   bool operator==(const File &other) const noexcept;
+  bool operator<(const File &rhe) const noexcept;
+
+  struct RefComparer
+  {
+    bool operator()(const std::reference_wrapper<const File> &lhs,
+                    const std::reference_wrapper<const File> &rhs) const;
+  };
 };
 
 } // namespace core
--- src/file.cpp
+++ src/file.cpp
@@ -12,4 +12,16 @@ bool File::operator==(const File &other) const noexcept
   return other.path == this->path;
 }
 
+bool File::operator<(const File &rhe) const noexcept
+{
+  return this->path < rhe.path;
+}
+
+bool File::RefComparer::operator()(
+    const std::reference_wrapper<const File> &lhs,
+    const std::reference_wrapper<const File> &rhs) const
+{
+  return lhs.get() < rhs.get();
+}
+
 } // namespace core
